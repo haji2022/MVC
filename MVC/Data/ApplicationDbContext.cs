dbb80078c2ba72528378a0a55841886485b6097d
Abdirahman <Abdirahman@DESKTOP-094P6TT>
2022-09-14 15:21:02 +0200

    Start

--- MVC/Data/ApplicationDbContext.cs
+++ MVC/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MVC.Models;
 using MVC.ViewModels;
 namespace MVC.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext()
         { }
